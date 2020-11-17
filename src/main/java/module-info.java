3355e916c7c040a8bb6c2158232fa489f2d5398e
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-11-17 16:02:53 +0700

    fix error Illegal reflective access by com.jfoenix.adapters.ReflectionHelper

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module HuffmanCoding {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
 
     opens org.openjfx to javafx.fxml;
     exports org.openjfx;
--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,12 +1,23 @@
 package org.openjfx;
 
 import java.io.IOException;
+
+
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
+
 public class PrimaryController {
+    @FXML private JFXTextField inputText;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void onButtonPressed() {
+        inputText.clear();
+        System.out.println(inputText.getText());
     }
 }
